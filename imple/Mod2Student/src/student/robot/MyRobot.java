7ed523e6ba8791904b3763cdd9656999102dcd99
dschwenkert <dschwenkert@gmail.com>
2016-07-15 12:47:06 +0200

    test

--- imple/Mod2Student/src/student/robot/MyRobot.java
+++ imple/Mod2Student/src/student/robot/MyRobot.java
@@ -1,11 +1,15 @@
 package student.robot;
 
+import java.beans.DesignMode;
 import java.io.Console;
+
+import com.sun.org.apache.xerces.internal.dom.DeferredElementNSImpl;
 
 import mod.network.IMessage;
 import mod.network.IMessageHandler;
 import mod.robot.Robot;
 import mod.robot.interfaces.ArrivalHandler;
+import robotino.Position;
 import student.Simulation;
 import student.common.DemoMessage;
 import student.common.Destination;
@@ -19,6 +23,7 @@ public class MyRobot extends Robot implements IMessageHandler, ArrivalHandler
 	{	
 		super(ip);
 		network.registerMessageHandler(this);
+		//drive.driveToPosition(new Position(0, 0), 1, this);
 	}
 
 	private Task currentTask = null;
@@ -29,8 +34,10 @@ public class MyRobot extends Robot implements IMessageHandler, ArrivalHandler
 	{
 		System.out.printf("Robot %s: Starting...\n", address());
 		
+		System.out.println("run : " + network.getMyID());
+		
 		//register at server
-		DemoMessage msg = new DemoMessage("hallo I am " + network.getMyID() ,SERVER_ID);
+		DemoMessage msg = new DemoMessage("hallo I am " + network.getMyID(),network.getMyID());
 		msg.setSenderID(network.getMyID());
 		network.send(msg);
 		
@@ -38,7 +45,7 @@ public class MyRobot extends Robot implements IMessageHandler, ArrivalHandler
 		while(!Thread.currentThread().isInterrupted() && connected())
 		{
 			processEvents();
-			System.out.println(network.getMyID());
+			//System.out.println("id" + network.getMyID());
 			Simulation.pause(50);
 		}
 		
@@ -60,11 +67,16 @@ public class MyRobot extends Robot implements IMessageHandler, ArrivalHandler
 	
 	@Override
 	public void handleIncommingMessage(IMessage message) {
-		System.out.println("robot got msg");
+		System.out.println("");
+		System.out.println("robot " + this.network.getMyID() + " got msg");
 		if(message.getClass() == Task.class){
 			currentTask = (Task) message;
 			taskPtr = 0;
 			continueTask();
+		}
+		else if(message.getClass() == DemoMessage.class){
+			DemoMessage demoMsg = (DemoMessage) message;
+			System.out.println(demoMsg.str);
 		}
 	}
 
