e3701c0925fd25cd1df68583ad91d7aac53684ad
dschwenkert <dschwenkert@gmail.com>
2016-07-19 16:34:03 +0200

    clean up

--- imple/Mod2Student/src/student/robot/MyRobot.java
+++ imple/Mod2Student/src/student/robot/MyRobot.java
@@ -18,56 +18,41 @@ import student.common.Task;
 
 import java.util.concurrent.TimeUnit ;
 
-public class MyRobot extends Robot implements IMessageHandler, ArrivalHandler
+public class MyRobot extends Robot
 {
-	public static final int SERVER_ID = 3;
 	
 	public MyRobot(String ip)
 	{	
 		super(ip);
-		network.registerMessageHandler(this);
-		//drive.driveToPosition(new Position(0, 0), 1, this);
+
+		//DEMO:
+		drive.driveToPosition(new Position(0, 0), 1, new ArrivalHandler() {
+			
+			@Override
+			public void arrived() {
+				
+				
+			}
+		});
 	}
 
-	private Task currentTask = null;
-	private int taskPtr = 0;
 	
 	@Override
 	public void run()
 	{
-		System.out.printf("Robot %s: Starting...\n", address());
-		
-		System.out.println("run : " + network.getMyID());
-		
 		//register at server
-		DemoMessage msg = new DemoMessage("hallo I am " + network.getMyID(),network.getMyID());
-		msg.setSenderID(network.getMyID());
-		network.send(msg);
-		drive.driveToPosition(new Position(0, 0), 0.5f, this);
+
 		
 		
 		while(!Thread.currentThread().isInterrupted() && connected())
 		{
 			processEvents();
-			//System.out.println("id" + network.getMyID());
 			Simulation.pause(50);
 		}
 		
 		System.out.printf("Robot %s: Terminating...\n", address());
 	}
 
-	private void continueTask(){
-		if(currentTask.pos.size() <= taskPtr){
-			//finished
-			System.out.println("task finished");
-		}
-		else{
-			Destination dest = currentTask.pos.get(taskPtr++);
-			if(dest.speed == Destination.Speed.Fast){
-				drive.driveToPosition(dest.position, 1, this);
-			}
-		}
-	}
 	
 	private void driveAroundObstacle() {
 		float s = 0.5f;				//Sicherheitsabstand
@@ -102,26 +87,6 @@ public class MyRobot extends Robot implements IMessageHandler, ArrivalHandler
 				d += 0.1f;
 			}
 		}
-	}
-	
-	@Override
-	public void handleIncommingMessage(IMessage message) {
-		System.out.println("");
-		System.out.println("robot " + this.network.getMyID() + " got msg");
-		if(message.getClass() == Task.class){
-			currentTask = (Task) message;
-			taskPtr = 0;
-			continueTask();
-		}
-		else if(message.getClass() == DemoMessage.class){
-			DemoMessage demoMsg = (DemoMessage) message;
-			System.out.println(demoMsg.str);
-		}
-	}
-
-	@Override
-	public void arrived() {
-		
 	}
 
 }
